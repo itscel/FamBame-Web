53f7eb25c13c758bb5383c95b426ccaedabecea4
itscel <cerilheyrosa@gmail.com>
2026-02-22 13:51:53 +0800

    First: HTML

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const button = document.getElementById("loadReact");
+
+button?.addEventListener("click", () => {
+  const root = ReactDOM.createRoot(
+    document.getElementById("react-root")!
+  );
+
+  root.render(<App />);
+});
